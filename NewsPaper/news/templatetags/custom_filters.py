f27e5ee7cce09630f820c93fd40c1074887281ed
userTany0013 <tany1ghom@gmail.com>
2024-10-22 12:38:32 +0300

    D6.4

--- NewsPaper/news/templatetags/custom_filters.py
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -3,19 +3,13 @@ from django import template
 
 register = template.Library()
 
-a1 = "Редиска"
-a2 = "редиска"
 
-a = [a1, a2]
+a = ["Редиска", "редиска"]
 
 @register.filter()
 def currency(value):
-    value = value.split()
-    for a_ in a:
-        if a_ in value:
-            for _a in a_[1:-0]:
-                _a = "*"
-    value = " ".join(value)
+    for i in a:
+        value = value.replace(i[1:], "*" * len(i[1:]))
     return value
 
 
